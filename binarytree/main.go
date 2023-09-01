78e1d4b29c543833b8dcec5d2810929f343e0488
zfl <2759025860@qq.com>
2023-09-01 20:19:07 +0800

    最近刷题，二叉树

---
+++ binarytree/main.go
@@ -0,0 +1,252 @@
+package main
+
+import (
+	"container/list"
+	"strconv"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+
+var rs int
+var maxD int
+
+func findBottomLeftValue(root *TreeNode) int {
+	rs, maxD = 0, 0
+	dfsFindBottomLeftValue(root, 0)
+	return rs
+}
+func dfsFindBottomLeftValue(root *TreeNode, d int) {
+	if root == nil {
+		return
+	}
+	if d > maxD {
+		maxD = d
+		rs = root.Val
+	}
+	dfsFindBottomLeftValue(root.Left, d+1)
+	dfsFindBottomLeftValue(root.Right, d+1)
+}
+func sumOfLeftLeaves(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 0
+	}
+	leftValue := sumOfLeftLeaves(root.Left)
+	if root.Left != nil && root.Left.Left == nil && root.Left.Right == nil {
+		leftValue = root.Left.Val
+	}
+	rightValue := sumOfLeftLeaves(root.Right)
+	return leftValue + rightValue
+}
+
+func getHeight(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftH := getHeight(root.Left)
+	rightH := getHeight(root.Right)
+	if leftH == -1 || rightH == -1 {
+		return -1
+	}
+	target := leftH - rightH
+	if target > 1 || target < -1 {
+		return -1
+	}
+	return max(leftH, rightH) + 1
+}
+func isBalanced(root *TreeNode) bool {
+	rs := getHeight(root)
+	if rs == -1 {
+		return false
+	}
+	return true
+}
+func binaryTreePaths(root *TreeNode) []string {
+	rs := make([]string, 0)
+	var travel func(node *TreeNode, path string)
+	travel = func(root *TreeNode, path string) {
+		path += strconv.Itoa(root.Val)
+		if root.Left == nil && root.Right == nil {
+			rs = append(rs, path)
+			return
+		}
+		if root.Left != nil {
+			path += "->"
+			travel(root.Left, path)
+		}
+		if root.Right != nil {
+			path += "->"
+			travel(root.Right, path)
+		}
+	}
+	travel(root, "")
+	return rs
+}
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftH, rightH := 0, 0
+	leftNode := root.Left
+	rightNode := root.Right
+	for leftNode != nil {
+		leftNode = leftNode.Left
+		leftH++
+	}
+	for rightNode != nil {
+		rightNode = rightNode.Right
+		rightH++
+	}
+	if leftH == rightH {
+		return 2<<leftH - 1
+	}
+	return countNodes(root.Left) + countNodes(root.Right) + 1
+}
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left != nil && root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+	if root.Right != nil && root.Right == nil {
+		return minDepth(root.Right) + 1
+	}
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Right), maxDepth(root.Left)) + 1
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func dfsSymmetric(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	} else if left == nil || right == nil {
+		return false
+	} else if left.Val != right.Val {
+		return false
+	}
+	return dfsSymmetric(left.Right, right.Left) && dfsSymmetric(left.Left, right.Right)
+}
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return dfsSymmetric(root.Right, root.Left)
+}
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	rs := make([][]int, 0)
+	var tmp []int
+	queue := make([]*TreeNode, 1)
+	queue[0] = root
+	for len(queue) > 0 {
+		length := len(queue)
+		for i := 0; i < length; i++ {
+			cur := queue[i]
+			tmp = append(tmp, cur.Val)
+			if cur.Left != nil {
+				queue = append(queue, cur.Left)
+			}
+			if cur.Right != nil {
+				queue = append(queue, cur.Right)
+			}
+		}
+		queue = queue[length:]
+		rs = append(rs, tmp)
+		tmp = []int{}
+	}
+	return rs
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	rs := make([]int, 0)
+	var st []*TreeNode
+	cur := root
+	for len(st) > 0 || cur != nil {
+		if cur != nil {
+			st = append(st, cur)
+			cur = cur.Left
+		} else {
+			cur = st[len(st)-1]
+			st = st[:len(st)-1]
+			rs = append(rs, cur.Val)
+			cur = cur.Right
+		}
+	}
+	return rs
+}
+
+// 迭代
+func preorderTraversal(root *TreeNode) []int {
+	rs := make([]int, 0)
+	if root == nil {
+		return rs
+	}
+	st := list.New()
+	st.PushBack(root)
+	for st.Len() > 0 {
+		node := st.Remove(st.Back()).(*TreeNode)
+		rs = append(rs, node.Val)
+		if node.Right != nil {
+			st.PushBack(node.Right)
+		}
+		if node.Left != nil {
+			st.PushBack(node.Left)
+		}
+	}
+	return rs
+}
+
+/*//递归
+func preorderTraversal(root *TreeNode) []int {
+	rs := make([]int, 0)
+	var traversal func(node *TreeNode)
+	traversal = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		rs = append(rs, node.Val)
+		traversal(node.Left)
+		traversal(node.Right)
+	}
+	traversal(root)
+	return rs
+}
+*/
--- stackandqueue/main.go
+++ stackandqueue/main.go
@@ -1,7 +1,153 @@
 package main
 
+import (
+	"container/heap"
+	"strconv"
+)
+
 func main() {
 
+}
+func topKFrequent(nums []int, k int) []int {
+	m := map[int]int{}
+	rs := make([]int, k)
+
+	for _, num := range nums {
+		m[num]++
+	}
+	h := &IHeap{}
+	for key, value := range m {
+		heap.Push(h, [2]int{key, value})
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	for i := k - 1; i >= 0; i-- {
+		rs[i] = heap.Pop(h).([2]int)[0]
+	}
+	return rs
+}
+
+// 构建小顶堆 sort.Interface
+//
+//	Push(x any) // add x as element Len()
+//	Pop() any
+type IHeap [][2]int
+
+func (h IHeap) Len() int {
+	return len(h)
+}
+
+func (h IHeap) Less(i, j int) bool {
+	return h[i][1] < h[j][1]
+}
+func (h IHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *IHeap) Pop() (v any) {
+	*h, v = (*h)[:h.Len()-1], (*h)[h.Len()-1]
+	return
+}
+func (h *IHeap) Push(x interface{}) {
+	*h = append(*h, x.([2]int))
+}
+
+// 封装单调队列的方式解题
+type MyQueue struct {
+	queue []int
+}
+
+func NewMyQueue() *MyQueue {
+	return &MyQueue{
+		queue: make([]int, 0),
+	}
+}
+
+func (m *MyQueue) front() int {
+	return m.queue[0]
+}
+
+func (m *MyQueue) back() int {
+	return m.queue[len(m.queue)-1]
+}
+
+func (m *MyQueue) pop(val int) {
+	if len(m.queue) > 0 && m.front() == val {
+		m.queue = m.queue[1:]
+	}
+}
+func (m *MyQueue) push(val int) {
+	for i := len(m.queue) - 1; i >= 0; i-- {
+		if val > m.queue[i] {
+			m.queue = m.queue[:len(m.queue)-1]
+		} else {
+			break
+		}
+	}
+	m.queue = append(m.queue, val)
+}
+func maxSlidingWindow(nums []int, k int) []int {
+	q := NewMyQueue()
+	rs := make([]int, 0)
+	for i := 0; i < k; i++ {
+		q.push(nums[i])
+	}
+	rs = append(rs, q.front())
+	for i := k; i < len(nums); i++ {
+		q.pop(nums[i-k])
+		q.push(nums[i])
+		rs = append(rs, q.front())
+	}
+	return rs
+}
+func evalRPN(tokens []string) int {
+	stack := []int{}
+	for _, token := range tokens {
+		val, err := strconv.Atoi(token)
+		if err != nil {
+			num1 := stack[len(stack)-1]
+			num2 := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			switch token {
+			case "+":
+				stack = append(stack, num2+num1)
+			case "-":
+				stack = append(stack, num2-num1)
+			case "*":
+				stack = append(stack, num2*num1)
+			case "/":
+				stack = append(stack, num2/num1)
+			}
+		} else {
+			stack = append(stack, val)
+		}
+	}
+	return stack[0]
+}
+func removeDuplicates(s string) string {
+	stack := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		if len(stack) > 0 && stack[len(stack)-1] == s[i] {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+	return string(stack)
+}
+func isValid(s string) bool {
+	hash := map[byte]byte{')': '(', ']': '[', '}': '{'}
+	stack := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
+			stack = append(stack, s[i])
+		} else if len(stack) > 0 && stack[len(stack)-1] == hash[s[i]] {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
 }
 
 type MyStack struct {
@@ -39,23 +185,23 @@ func (this *MyStack) Empty() bool {
 	return len(this.queue) == 0
 }
 
-type MyQueue struct {
+/*type MyQueue struct {
 	stackIn  []int
 	stackOut []int
 }
 
-/*func Constructor() MyQueue {
+func Constructor() MyQueue {
 	return MyQueue{
 		stackIn:  make([]int, 0),
 		stackOut: make([]int, 0),
 	}
-}*/
+}
 
-func (this *MyQueue) Push(x int) {
+func (this MyQueue) Push(x int) {
 	this.stackIn = append(this.stackIn, x)
 }
 
-func (this *MyQueue) Pop() int {
+func (this MyQueue) Pop() int {
 	inLen, outLen := len(this.stackIn), len(this.stackOut)
 	if outLen == 0 {
 		if inLen == 0 {
@@ -72,7 +218,7 @@ func (this *MyQueue) Pop() int {
 	return value
 }
 
-func (this *MyQueue) Peek() int {
+func (this MyQueue) Peek() int {
 	value := this.Pop()
 	if value == -1 {
 		return -1
@@ -81,9 +227,10 @@ func (this *MyQueue) Peek() int {
 	return value
 }
 
-func (this *MyQueue) Empty() bool {
+func (this MyQueue) Empty() bool {
 	return len(this.stackIn) == 0 && len(this.stackOut) == 0
 }
+*/
 
 // MyQueue 泛型实现队列
 /*type MyQueue[T interface{}] struct {
@@ -94,11 +241,11 @@ func Constructor() MyQueue[int] {
 	return MyQueue[int]{}
 }
 
-func (q *MyQueue[T]) put(value T) {
+func (q MyQueue[T]) put(value T) {
 	q.elements = append(q.elements, value)
 }
 
-func (q *MyQueue[T]) Pop() T {
+func (q MyQueue[T]) Pop() T {
 	var value T
 	if len(q.elements) > 0 {
 		value = q.elements[0]
@@ -108,7 +255,7 @@ func (q *MyQueue[T]) Pop() T {
 	return value
 }
 
-func (q *MyQueue[T]) Peek() T {
+func (q MyQueue[T]) Peek() T {
 	var value T
 	if len(q.elements) > 0 {
 		value = q.elements[0]
@@ -117,7 +264,7 @@ func (q *MyQueue[T]) Peek() T {
 	return value
 }
 
-func (q *MyQueue[T]) Empty() bool {
+func (q MyQueue[T]) Empty() bool {
 	if len(q.elements) == 0 {
 		return true
 	}
